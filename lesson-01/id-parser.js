a449b392bce75261f46a5d0c2c61cde7ac6381a2
Arswarog <arswarog@yandex.ru>
2020-05-16 16:23:36 +0300

    Lesson 2. Flight control system

--- lesson-01/id-parser.js
+++ lesson-01/id-parser.js
@@ -5,24 +5,26 @@
 // D идентификатор пользователя
 // Все входные данные - целые десятичные числа
 
-const id = '5eabb7d03a854bdff'
+parseId('5eabb7d03a854bdff');
 
-const a = id.substr(0, 8);
-const timestamp = parseInt(a, 16);
+function parseId(id) {
+    console.log('parsings', id);
+    const a = id.substr(0, 8);
+    const timestamp = parseInt(a, 16);
 
-const b = id.substr(8, 2);
-const claster = parseInt(b, 16);
+    const b = id.substr(8, 2);
+    const claster = parseInt(b, 16);
 
-const c = id.substr(10, 1);
-const type = parseInt(c, 16);
+    const c = id.substr(10, 1);
+    const type = parseInt(c, 16);
 
-const d = id.substr(11);
-const user = parseInt(d, 16);
+    const d = id.substr(11);
+    const user = parseInt(d, 16);
 
 
-console.log(a, b, c, d);
-console.log('timestamp :', timestamp, new Date(timestamp * 1000));
-console.log('claster :', claster);
-console.log('type :', type);
-console.log('user :', user);
-
+    console.log(a, b, c, d);
+    console.log('timestamp :', timestamp, new Date(timestamp * 1000));
+    console.log('claster :', claster);
+    console.log('type :', type);
+    console.log('user :', user);
+}
